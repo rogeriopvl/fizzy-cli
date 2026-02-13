16a74d984c0723e860bad20e172077215a8f4d19
rogeriopvl <rogerio@hey.com>
2026-02-13 15:01:32 +0000

    fix(config): update default permissions when creating file

--- internal/config/config.go
+++ internal/config/config.go
@@ -62,7 +62,7 @@ func (c *Config) Save() error {
 		return fmt.Errorf("marshaling config: %w", err)
 	}
 
-	if err := os.WriteFile(configPath, data, 0o644); err != nil {
+	if err := os.WriteFile(configPath, data, 0o600); err != nil {
 		return fmt.Errorf("writing config file: %w", err)
 	}
 
